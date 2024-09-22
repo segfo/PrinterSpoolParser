8d15f0b4540a5cf22f56fff20d51fe484a901102
segfo <k.segfo@gmail.com>
2024-09-22 21:44:05 +0900

    v1.0.0

---
+++ PrinterSpoolParser/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+using EMFSpool;
+
+namespace PrinterSpoolParseW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var argsDict = args.Select(arg => arg.Split('=')).Where(s => s.Length == 2).ToDictionary(v => v[0], v => v[1]);
+            var spool = argsDict.TryGetValue("spl",out var spl)? spl : "FP00000.SPL";
+            var output = argsDict.TryGetValue("OutputFile", out var opval) ? opval : "out";
+
+            FileStream spoolFileStream = new FileStream(spool, FileMode.Open, FileAccess.Read);
+            BinaryReader spoolReader = new BinaryReader(spoolFileStream, Encoding.Unicode);
+            EMFSpoolFile spoolFile = new EMFSpoolFile(spoolReader);
+            spoolFile.Pages[0].PageImage.Save(output+".png");
+            spoolFile.Pages[0].Thumbnail.Save(output+"_Thumbnail.png");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# なにこれ？
+Windowsスプールファイル(.spl)をpngでエクスポートするツールです。
+
+# ビルドの方法
+参照に、以下のDLLを追加してください。
+PrinterSpoolParse/EMFSpool.dll
+
+# ダウンロード
+リリースページからダウンロードしてください。
+
+# 使い方
+PrinterSpoolParse.exe
